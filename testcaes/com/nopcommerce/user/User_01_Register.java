efde5174039caa9a618ac8122ffce08f731186b4
Le Tai <ldtai1205@gmail.com>
2023-02-28 16:39:32 +0700

    LiveCode - NopCommerce

---
+++ testcaes/com/nopcommerce/user/User_01_Register.java
@@ -0,0 +1,41 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_Register {
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+  
+  @Test
+  public void Register_01_Empty_Data() {
+  }
+  
+  @Test
+  public void Register_02_Invalid_Email() {
+  }
+  
+  @Test
+  public void Register_03_Register_Success() {
+  }
+  
+  @Test
+  public void Register_04_Exsting_Email() {
+  }
+  
+  @Test
+  public void Register_05_Password_Less_Than_6_Chars() {
+  }
+  
+  @Test
+  public void Register_06_Invalid_Confirm_Password() {
+  }
+  	
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
